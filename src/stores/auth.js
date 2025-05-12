a7a8ebbc74bd6daa1cf6a19b7d6bdeeea2eb418b
정한슬 <hschung135@naver.com>
2025-05-12 16:46:58 +0900

    Refactor: Option API 표현 -> Composition API 표현으로 수정

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,24 +1,29 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore('auth', {
-    state: () => ({
-        isLoggedIn: false,
-        accessToken: null,
-    }),
-    actions: {
-        login(token) {
-        this.accessToken = token
-        this.isLoggedIn = true
-        },
-        logout() {
-        this.accessToken = null
-        this.isLoggedIn = false
-        },
-    },
-    persist: [
-    {
-        paths: ["auth"],
-        storage: localStorage,
-        },
-    ],
+export const useAuthStore = defineStore('auth', () => {
+    const isLoggedIn = ref(false)
+    const accessToken = ref(null)
+
+    function login(token) {
+        accessToken.value = token
+        isLoggedIn.value = true
+    }
+
+    function logout() {
+        accessToken.value = null
+        isLoggedIn.value = false
+    }
+
+    return {
+        isLoggedIn,
+        accessToken,
+        login,
+        logout
+    }
+    }, {
+    persist: {
+        paths: ['accessToken', 'isLoggedIn'],
+        storage: localStorage
+    }
 })
