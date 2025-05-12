40f01e3d2d3d52c02edd030215c2bff080eaeafd
정한슬 <hschung135@naver.com>
2025-05-12 17:30:17 +0900

    Style: 마이페이지 속 내 활동 내역 추가
    - USER, MEMBERSHIP은 내 장소 등록하기 버튼 추가
    - gpt 내역 확인하기 path 수정

--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -58,11 +58,13 @@ const router = useRouter();
 
 const allActions = {
   USER: [
+    { label: "내 장소 등록하기", path: "/mypage/places" },
     { label: "찜한 공고 보러가기", path: "/favorites" },
     { label: "내가 쓴 글 보러가기", path: "/myposts" },
   ],
   MEMBERSHIP: [
-    { label: "gpt 내역 확인하기기", path: "/gptlists" },
+    { label: "내 장소 등록하기", path: "/mypage/places" },
+    { label: "gpt 내역 확인하기", path: "/mypage/gpt" },
     { label: "찜한 공고 보러가기", path: "/favorites" },
     { label: "내가 쓴 글 보러가기", path: "/myposts" },
   ],
